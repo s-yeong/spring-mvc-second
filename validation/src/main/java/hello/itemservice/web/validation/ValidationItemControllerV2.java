29463a224a0dab7d0de4ecdf9e049021c05abbaa
s-yeong <syxxng25@gmail.com>
2023-01-12 17:24:29 +0900

    검증1 - Validation: 오류 코드와 메시지 처리6

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -206,6 +206,7 @@ public class ValidationItemControllerV2 {
 
         // 검증에 실패하면 다시 입력 폼으로
         if(bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
             return "validation/v2/addForm"; // redirect X
         }
 
