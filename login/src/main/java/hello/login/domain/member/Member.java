754e10d7c513f5104a24cb5a467ec2482a01b549
s-yeong <syxxng25@gmail.com>
2023-01-16 09:52:13 +0900

    로그인 처리1 - 쿠키, 세션: 회원 가입

---
+++ login/src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,19 @@
+package hello.login.domain.member;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class Member {
+
+    private Long id;    // DB에 저장되고 관리되는 ID
+
+    @NotEmpty
+    private String loginId; // 로그인 ID
+    @NotEmpty
+    private String name;    // 사용자 이름
+    @NotEmpty
+    private String password;
+
+}
