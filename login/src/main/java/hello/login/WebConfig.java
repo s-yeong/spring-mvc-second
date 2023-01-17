9f74472fafb29508fbd536fd6960554a42b00647
s-yeong <syxxng25@gmail.com>
2023-01-17 12:50:04 +0900

    로그인 처리2 - 필터, 인터셉터: 스프링 인터셉터 - 인증 체크

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -3,6 +3,7 @@ package hello.login;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,10 +21,14 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**") // 인터셉터 적용할 URL 패턴(하위 전부다)
                 .excludePathPatterns("/css/**", "/*.ico", "/error");    // 인터셉터에서 제외할 패턴
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/members/add", "/login", "/css/**", "/*.ico", "/error");
     }
 
-/*
-    @Bean
+//    @Bean
     public FilterRegistrationBean logFilter() {
         // WAS를 띄울 때 Filter를 같이 넣어줌
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
@@ -33,9 +38,8 @@ public class WebConfig implements WebMvcConfigurer {
 
         return filterFilterRegistrationBean;
     }
-*/
 
-    @Bean
+//    @Bean
     public FilterRegistrationBean loginCheckFilter() {
         // WAS를 띄울 때 Filter를 같이 넣어줌
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
