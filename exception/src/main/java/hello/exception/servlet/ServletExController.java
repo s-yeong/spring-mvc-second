6b6d2aa146ce9e3067354bce09feba14f1e17368
s-yeong <syxxng25@gmail.com>
2023-01-17 20:34:28 +0900

    예외 처리와 오류 페이지: 서블릿 예외 처리 - 시작

---
+++ exception/src/main/java/hello/exception/servlet/ServletExController.java
@@ -0,0 +1,31 @@
+package hello.exception.servlet;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ServletExController {
+
+    @GetMapping("/error-ex")
+    public void errorEx() {
+        throw new RuntimeException("예외 발생");
+        // Exception 터지면 -> 서블릿 컨테이너는 무조건 "500"으로 나감
+    }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404 오류!");
+        // sendError -> 상태코드 지정 가능
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500);
+    }
+
+}
